dac3ccba831d1c1481bac25f16e2fe578191d78c
Govardhan K G <govi.reddy420@gmail.com>
2026-03-04 00:16:23 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import { ThemeProvider } from './providers'
 
@@ -41,7 +40,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
-        <Analytics />
+        
       </body>
     </html>
   )
